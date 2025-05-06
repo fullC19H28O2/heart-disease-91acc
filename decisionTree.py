84872adc5cb62369b804b72e000ec9120a5f48c4
Halo <101888522+fullC19H28O2@users.noreply.github.com>
2025-05-06 14:36:19 +0300

    ml methods applied

---
+++ decisionTree.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+df = pd.read_csv("Heart_disease_cleveland_new.csv")
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train_scaled, y_train)
+
+y_pred = model.predict(X_test_scaled)
+
+print("decision tree doğruluk:", accuracy_score(y_test, y_pred))
+print("\n Sınıflandırma Raporu:\n")
+print(classification_report(y_test, y_pred))
